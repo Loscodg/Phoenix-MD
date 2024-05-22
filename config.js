5dc104b213ef8effb5be60b01abcd1d81f7c121e
Loscodg <leeslee06@gmail.com>
2024-05-22 12:28:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -12,15 +12,15 @@ module.exports = {
   //For Enabling Commands Like AUTO_STATUS_RED Type true For Disenabling Type false
   ANTILINK: toBool(process.env.ANTI_LINK) || true,
   //_________________________________________________________________________________________________________________________________
-  LOGS: toBool(process.env.LOGS) || true,
+  LOGS: toBool(process.env.LOGS) || false,
   //_________________________________________________________________________________________________________________________________
   ANTILINK_ACTION: process.env.ANTI_LINK || "kick",
   //_________________________________________________________________________________________________________________________________
-  AUTO_REACT: process.env.AUTO_REACT || 'true',
+  AUTO_REACT: process.env.AUTO_REACT || 'false',
   //_________________________________________________________________________________________________________________________________
   AUDIO_DATA: process.env.AUDIO_DATA || "Phoenix-MD;Abhishek Suresh;https://graph.org/file/8976892f2f615077b48cd.jpg",
   //_________________________________________________________________________________________________________________________________
-  AUTO_STATUS_READ: process.env.AUTO_STATUS_READ || 'true',
+  AUTO_STATUS_READ: process.env.AUTO_STATUS_READ || 'false',
   //_________________________________________________________________________________________________________________________________
   SESSION_ID: process.env.SESSION_ID || "Phoenix~SACJNK1L", //Enter Your Session Id Here
   //_________________________________________________________________________________________________________________________________
